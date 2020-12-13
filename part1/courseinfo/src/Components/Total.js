979483b4e8d8b5d5a39b6073d620401668598d75
lucadavid075 <daiveedlucas049@gmail.com>
2020-12-13 02:53:56 +0100

    finished step 3 of Exercises 1.3 - 1.5

--- part1/courseinfo/src/Components/Total.js
+++ part1/courseinfo/src/Components/Total.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Total(props) {
-   
+   console.log(props);
     return(
         <div>
             <p>Total number of exercises are {props.exercises1 + props.exercises2 + props.exercises3}</p>
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,14 +5,25 @@ import Content from './Components/Content'
 import Total from './Components/Total'
 
 
-
-
 const App = () => {
+  const course ="Half Stack application development"
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
-      <Header course= 'Half Stack application development' />
-      <Content />
-      <Total exercises1={10} exercises2={7} exercises3={14} />
+      <Header course={course} />
+      <Content parts={part1.name} exercises={part1.exercises} />
+      <Total exercises1={part1.exercises}  exercises2={part2.exercises}  exercises3={part3.exercises} />
     </div>
   );
 }
